52b05b89c5983c2db1f5bddb6d03794f229cb20c
bent0 <45747598+bent0000@users.noreply.github.com>
2022-11-15 02:03:44 +0700

    Update nibiru.sh

--- nibiru.sh
+++ nibiru.sh
@@ -9,7 +9,7 @@ echo "      ###   ###    ##          ###   ####       ##       ##      ##";
 echo "      ###     ##   #########   ###    ###       ##       ##########";
 echo "      ##########   #########   ###    ###       ##       ##########";
 
-echo -e '\e[36mTwitter:\e[39m' https://twitter.com/yuwanda_arfi
+echo -e '\e[36mTwitter:\e[39m' https://twitter.com/Kyozer_er
 echo -e '\e[36mGithub: \e[39m' https://github.com/bent0000
 echo -e "\e[0m"
 
